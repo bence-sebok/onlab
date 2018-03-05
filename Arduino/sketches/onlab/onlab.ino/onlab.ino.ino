490b45c89a5488d660b1dc95825b35313f0ec751
Sebők Bence <sebok.bence.003@gmail.com>
2018-03-05 21:50:31 +0100

    Arduino szenzorok

--- Arduino/sketches/onlab/onlab.ino/onlab.ino.ino
+++ Arduino/sketches/onlab/onlab.ino/onlab.ino.ino
@@ -2,38 +2,56 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 #include <BH1750.h>
+#include <DHT.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
 
 int flag_meres = 0;
 uint32_t counter = 0;
+uint32_t meres_sorszam = 1;
+int seconds = 0;
    
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 BH1750 lightMeter;
+DHT dht(DHTPIN, DHTTYPE);
 
-// TIMER0 interrupt 2 kHz
-ISR(TIMER0_COMPA_vect)
+// #define DEBUG
+
+ISR(TIMER1_COMPA_vect)
 {
-  //Serial.print(counter); 
-  //Serial.println("");
-  counter++;
+  seconds++;
+}
+
+void setTimer1(void)
+{
+  // initialize Timer1
+  cli();          // disable global interrupts
+  TCCR1A = 0;     // set entire TCCR1A register to 0
+  TCCR1B = 0;     // same for TCCR1B
+  
+  // set compare match register to desired timer count:
+  OCR1A = 15624; // 15624
+  
+  // turn on CTC mode:
+  TCCR1B |= (1 << WGM12);
+  
+  // Set CS10 and CS12 bits for 1024 prescaler:
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12);
+  
+  // enable timer compare interrupt:
+  TIMSK1 |= (1 << OCIE1A);
+  
+  // enable global interrupts:
+  sei();
 }
 
 void setup(void) 
 {
-  cli();
-  //set timer0 interrupt at 2kHz
-  TCCR0A = 0;// set entire TCCR2A register to 0
-  TCCR0B = 0;// same for TCCR2B
-  TCNT0  = 0;//initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS02) | (1 << CS00);   
-  // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
-  sei();
-  
+  setTimer1();
   Serial.begin(9600);
   Serial.println("Onallo laboratorium");
   
@@ -44,14 +62,46 @@ void setup(void)
     Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
+
+  dht.begin();
+}
+
+void dht11_read(void)
+{
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    // Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(h); // százalékos érték!!!
+  Serial.print(" ");
+  Serial.print(t);
+  Serial.print(" ");
+  Serial.print(hic);
+  Serial.println(" ");
 }
 
 void loop(void) 
 {
-  sensors_event_t event;
-  bmp.getEvent(&event);
-  if(counter >= 7500)
+  if(seconds >= 60)
   {
+    Serial.print(meres_sorszam);
+    Serial.print(" ");
+    sensors_event_t event;
+    bmp.getEvent(&event);
+    uint16_t lux = lightMeter.readLightLevel();
+    Serial.print(lux);
+    Serial.print(" ");
     if (event.pressure)
     {
       Serial.print(event.pressure); Serial.print(" ");
@@ -61,14 +111,10 @@ void loop(void)
     }
     else
     {
-      Serial.println("BMP180 sikertele meres");
+      Serial.println("BMP180 sikertelen meres");
     }
-    delay(50);
-  
-    uint16_t lux = lightMeter.readLightLevel();
-    Serial.print(lux);
-    Serial.println("");
-    delay(50);
-    counter = 0;
+    dht11_read();
+    meres_sorszam++;
+    seconds = 0;
   }
 }
