3bf6f1af5af34c878ea217b12abe1990f95c0a43
Sebők Bence <sebok.bence.003@gmail.com>
2018-03-05 22:20:36 +0100

    Matlab feldolgozás
    Adatszerkezet és ábrák frissítése

---
+++ Matlab/onlab_arduino_meres.m
@@ -0,0 +1,51 @@
+% Önálló laboratórium
+% Arduino mérési adatok feldolgozása
+
+clear;
+clc;
+
+% Mérési adatok fájlja
+fajlnev = 'onlab_arduino_meres.csv';
+meres = csvread(fajlnev, 1, 0);
+
+% Mérési adatok csoportosítása
+t = meres(:,1); % mérés sorszáma (percenként egy mérés)
+fenyerosseg = meres(:,2); % fényer?sség [lux]
+legnyomas = meres(:,3); % légnyomás [hPa]
+paratartalom = meres(:,5);  % páratartalom [%]
+% H?mérsékletek
+homerseklet_bmp180 = meres(:,4);  % BMP180 szenzor
+homerseklet_dht11 = meres(:,6);  % DHT11
+homerseklet_dht11_hic = meres(:,7);  % DHT11 heat index Celsiusban
+
+%% Els? ábra: fényer?sség, légnyomás, páratartalom
+figure(1)
+%%% Légnyomás
+subplot(3,1,1)
+plot(t, legnyomas)
+title('Legnyomas')
+xlabel('ido [s]')
+ylabel('legnyomas [hPa]')
+%%% Fényer?sség
+subplot(3,1,2)
+plot(t, fenyerosseg)
+title('Fenyerosseg')
+xlabel('ido [s]')
+ylabel('fenyerosseg [lux]')
+%%% Páratartalom
+subplot(3,1,3)
+plot(t, paratartalom)
+title('Paratartalom')
+xlabel('ido [s]')
+ylabel('paratartalom [%]')
+
+%% Második ábra: h?mérsékletek
+figure(2)
+hold all
+plot(t, homerseklet_bmp180)
+plot(t, homerseklet_dht11)
+plot(t, homerseklet_dht11_hic)
+title('Homerseklet')
+xlabel('ido [s]')
+ylabel('homerseklet [°C]')
+legend('homerseklet bmp180', 'homerseklet dht11', 'homerseklet dht11 hic')
